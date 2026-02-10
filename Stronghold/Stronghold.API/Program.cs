ef03fbf5a1b30f8ad8a610a79ad8169c5457511d
samamostar97 <samir.obradovic@outlook.com>
2026-02-10 02:42:41 +0100

    harden docker-compose for production deployment

--- Stronghold/Stronghold.API/Program.cs
+++ Stronghold/Stronghold.API/Program.cs
@@ -1,9 +1,5 @@
-using DotNetEnv;
 using Stronghold.API.Extensions;
 using Stronghold.API.Middleware;
-
-if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") != "true")
-    Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
