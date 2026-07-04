52ddf3047910c60e875e3033c3c993ce4a3ba336
samamostar97 <49598613+samamostar97@users.noreply.github.com>
2026-07-04 18:38:47 +0200

    Search payments by member

--- Stronghold.Application/DTOs/Payments/PaymentSearch.cs
+++ Stronghold.Application/DTOs/Payments/PaymentSearch.cs
@@ -5,6 +5,8 @@ namespace Stronghold.Application.DTOs.Payments;
 public class PaymentSearch : BaseSearchObject
 {
     public int? UserId { get; set; }
+    // Pretraga po imenu, prezimenu ili korisnickom imenu clana.
+    public string? Text { get; set; }
     public DateTime? From { get; set; }
     public DateTime? To { get; set; }
 }
